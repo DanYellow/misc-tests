ff042570a98e26cc6e067466bcc1d70cb45ff32c
DanYellow <jeanlouis.danielo@sfr.fr>
2017-11-18 21:46:15 -0500

    Add hot-reload to browserify

--- webpack-presentation-examples/browserify-sample/src/main.js
+++
@@ -1,9 +0,0 @@
-const factorial = require('./modules/factorial');
-
-const input = document.getElementById('factorialy');
-const resultContainer = document.getElementById('result');
-
-input.addEventListener('input', (e) => {
-    const value = e.currentTarget.value;
-    resultContainer.innerHTML = `${factorial(~~value)} possible combinations`;
-});
