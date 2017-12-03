3598b953585acf77d16e1875215fa6118c3c5963
DanYellow <jeanlouis.danielo@sfr.fr>
2017-12-02 21:26:56 -0500

    Adds multiple example for async loading

--- webpack-presentation-examples/webpack-samples/code-splitting/src/main.js
+++ webpack-presentation-examples/webpack-samples/code-splitting/src/main.js
@@ -2,9 +2,11 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 
 import './reset.css'
-import HelloMessage from './components/hello-message'
+// import HelloMessage from './components/hello-message'
+import HelloMessage from './components/react-async-hello-message'
+// import HelloMessage from './components/async-hello-message'
 
 ReactDOM.render(
-    <HelloMessage name="Québec !" />,
-    document.getElementById('app')
-  );
+  <HelloMessage name="Québec !" />,
+  document.getElementById('app')
+);
